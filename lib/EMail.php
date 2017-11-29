ae73eafec04e6981a2af74f3b7fc1cf6d6737483
Micha Rohde <hi@mi-roh.de>
2017-11-29 17:11:20 +0100

    rebuild to instance-Class

---
+++ lib/EMail.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: micharohde on 29.11.17 16:47
+ */
+
+namespace MIROH\email;
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+class EMail {
+
+    private $_styles = array();
+    private $_globalKey = 'miRoh_generateEmail_styles';
+
+    protected $_shortCode = 'email';
+
+    protected static $_SALT = 'G0uLUxcWeYRFpoYTMVOq';
+
+
+    /**
+     * @var EMail
+     */
+    private static $instance;
+
+    /**
+     * Setup the instance.
+     *
+     * @return EMail
+     */
+    public static function get_instance() {
+
+        if ( ! isset( self::$instance ) ) {
+            self::$instance = new self;
+            self::$instance->setup_actions();
+        }
+
+        return self::$instance;
+    }
+
+    public function setup_actions() {
+
+        if ( !isset( $GLOBALS[ $this->_globalKey ] ) ) {
+            $GLOBALS[ $this->_globalKey ] = array();
+        }
+
+        add_shortcode( $this->_shortCode, array( $this, '_shortCode' ) );
+        add_action( 'wp_footer', array( $this, '_footerStyle' ) );
+
+    }
+
+    public function _shortCode($data) {
+        $email = array_shift($data);
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return '';
+        }
+        return $this->_generateEmail($email);
+    }
+
+    public function _footerStyle() {
+
+        if (!empty($this->_styles)) {
+            echo '<style>';
+            echo '.miRoh-email {unicode-bidi:bidi-override; direction: rtl;white-space:nowrap;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;-o-user-select: none;user-select: none;}';
+            foreach ($this->_styles as $style) {
+                echo $style;
+            }
+            echo '</style>';
+        }
+    }
+
+    private function _generateEmail($email) {
+        list($user, $domain) = explode('@', $email);
+        $key = sha1 (strrev($user) . 'aAdress' . strrev($domain) . static::$_SALT . rand( 10000, 99999 ) );
+
+        $this->_styles[] = '.miRoh-email' . $key . ':after {content:\'' . strrev($user) . '\'}';
+        $this->_styles[] = '.miRoh-email' . $key . ':before {content:\'' . strrev($domain) . '\'}';
+
+        return'<span class="miRoh-email miRoh-email' . $key . '">&#064;</span>';
+    }
+    
+    public function addEmail( $email ) {
+        return $this->_generateEmail( $email );
+    }
+
+
+}
--- wp-shortcode-email.php
+++ wp-shortcode-email.php
@@ -15,60 +15,7 @@ License URI: https://github.com/mi-roh/wp-shortcode-email/blob/master/LICENSE
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-if (!class_exists('MiRoh_Email', false)) {
-    class MiRoh_Email {
+require_once __DIR__ . '/lib/EMail.php';
 
-        private $_headStyles = array();
-        private $_globalKey = 'miRoh_email_headStyles';
+MIROH\email\EMail::get_instance();
 
-        public static $SALT = 'G0uLUxcWeYRFpoYTMVOq';
-
-        public function __construct() {
-
-            if (!isset($GLOBALS[$this->_globalKey])) {
-                $GLOBALS[$this->_globalKey] = array();
-                add_shortcode('email', array(__CLASS__, 'embed'));
-                add_action('wp_footer', array(__CLASS__, 'headStyle'));
-            }
-            $this->_headStyles = &$GLOBALS[$this->_globalKey];
-
-        }
-
-        static public function embed($data) {
-            $email = array_shift($data);
-
-            $self = new self();
-
-
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                return '';
-            }
-            return $self->_email($email);
-        }
-
-        static public function headStyle($a = false, $b = false) {
-            $self = new self();
-            if (!empty($self->_headStyles)) {
-                echo '<style>';
-                echo '.miRoh-email {unicode-bidi:bidi-override; direction: rtl;white-space:nowrap;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;-o-user-select: none;user-select: none;}';
-                foreach ($self->_headStyles as $style) {
-                    echo $style;
-                }
-                echo '</style>';
-            }
-        }
-
-        private function _email($email) {
-            list($user, $domain) = explode('@', $email);
-            $key = sha1 (strrev($user) . 'aAdress' . strrev($domain) . static::$SALT . rand( 10000, 99999 ) );
-
-            $this->_headStyles[] = '.miRoh-email' . $key . ':after {content:\'' . strrev($user) . '\'}';
-            $this->_headStyles[] = '.miRoh-email' . $key . ':before {content:\'' . strrev($domain) . '\'}';
-            return'<span class="miRoh-email miRoh-email' . $key . '">&#064;</span>';
-        }
-
-
-    }
-}
-
-new MiRoh_Email();
